369afad0bd44975689b761fa50d446eb9df52120
Jamie S <jamieshi2u@gmail.com>
2019-02-07 19:37:36 -0800

    Fix typo

--- routes/login.js
+++ routes/login.js
@@ -3,5 +3,7 @@
  */
 
 exports.view = function (req, res) {
-  res.render('login');
+  res.render('login', {
+    navbarTitle: 'Login'
+  });
 };
--- routes/profile.js
+++ routes/profile.js
@@ -3,5 +3,7 @@
  */
 
 exports.view = function (req, res) {
-  res.render('profile');
+  res.render('profile', {
+    navbarTitle: 'Profile'
+  });
 };
