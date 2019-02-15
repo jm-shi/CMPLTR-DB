cdcf690eb23290a2172259d6cde8fc06cfe988ab
Andrew Nguyen <ahn018@ucsd.edu>
2019-02-14 20:59:49 -0800

    Updated login and profile

--- routes/login.js
+++ routes/login.js
@@ -1,9 +1,30 @@
-/*
- * GET login page.
- */
+// /*
+//  * GET login page.
+//  */
 
 exports.view = function (req, res) {
+  console.log("Inside login function");
   res.render('login', {
     navbarTitle: 'Login'
   });
 };
+
+// $(document).ready(function() {
+//   $('#username').focus();
+
+//   $('.login').click(function() {
+
+//       event.preventDefault(); // prevent PageReLoad
+
+//      var ValidEmail = $('#exampleInputEmail1').val() === 'invitado'; // User validate
+//     var ValidPassword = $('#exampleInputPassword1').val() === 'hgm2015'; // Password validate
+
+//       if (ValidEmail === true && ValidPassword === true) { // if ValidEmail & ValidPassword
+//           $('.valid').css('display', 'block');
+//           window.location = "/"; // go to home.html
+//       }
+//       else {
+//           $('.error').css('display', 'block'); // show error msg
+//       }
+//   });
+// });
