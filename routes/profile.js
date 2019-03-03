b372e199fbe62c07e2c6e183407b8bd50a2a2660
Jamie S <jamieshi2u@gmail.com>
2019-03-03 00:18:19 -0800

    Retrieve user data from MongoDB and display stats on profile

--- routes/profile.js
+++ routes/profile.js
@@ -2,57 +2,70 @@
  * GET profile page.
  */
 
-let currentRoutines = require('../currentRoutines.json');
-let previousRoutines = require('../previousRoutines.json');
+const { Routine } = require('../models/routine.js');
 
 exports.view = function (req, res) {
-  let currRoutinesDaysCompleted = 0;
-  let currRoutinesDaysTotal = 0
-  let currGoalsTotal = 0;
-  const currRoutines = currentRoutines.routines;
-  const numCurrRoutines = currRoutines.length;
-  for (let i = 0; i < numCurrRoutines; i++) {
-    const currRoutine = currRoutines[i];
-    currRoutinesDaysCompleted += currRoutine.daysCompleted;
-    currRoutinesDaysTotal += parseInt(currRoutine.daysToComplete);
-    currGoalsTotal += currRoutine.goals.length;
+  let signedInUser = 'anonymous';
+  if (req.session && req.session._id) {
+    signedInUser = req.session._id;
   }
 
-  let prevRoutinesDaysCompleted = 0;
-  let prevRoutinesDaysTotal = 0
-  let prevGoalsCompleted = 0;
-  let prevGoalsTotal = 0;
-  const prevRoutines = previousRoutines.routines;
-  const numPrevRoutines = prevRoutines.length;
-  for (let i = 0; i < numPrevRoutines; i++) {
-    const prevRoutine = prevRoutines[i];
-    prevRoutinesDaysCompleted += prevRoutine.daysCompleted;
-    prevRoutinesDaysTotal += parseInt(prevRoutine.daysToComplete);
-    prevGoalsCompleted += prevRoutine.completedGoalsCount;
-    prevGoalsTotal += prevRoutine.goals.length;
-  }
-  const prevGoalsFailed = prevGoalsTotal - prevGoalsCompleted;
+  let currentRoutines = [];
+  let previousRoutines = [];
+  Routine.find({ isArchived: false, owner: signedInUser }, function (err, userCurrRoutines) {
+    currentRoutines = userCurrRoutines;
+  }).then(function () {
+    Routine.find({ isArchived: true, owner: signedInUser }, function (err, userPrevRoutines) {
+      previousRoutines = userPrevRoutines;
+    }).then(function () {
+      let currRoutinesDaysCompleted = 0;
+      let currRoutinesDaysTotal = 0
+      let currGoalsTotal = 0;
+      const numCurrRoutines = currentRoutines.length;
+      for (let i = 0; i < numCurrRoutines; i++) {
+        const currRoutine = currentRoutines[i];
+        currRoutinesDaysCompleted += currRoutine.daysCompleted;
+        currRoutinesDaysTotal += parseInt(currRoutine.daysToComplete);
+        currGoalsTotal += currRoutine.goals.length;
+      }
 
-  const totalDaysCompleted = currRoutinesDaysCompleted + prevRoutinesDaysCompleted;
-  const totalDays = currRoutinesDaysTotal + prevRoutinesDaysTotal;
-  const totalDaysCompletedPercentage = totalDays === 0 ? 0 : Math.round((totalDaysCompleted / totalDays) * 100);
+      let prevRoutinesDaysCompleted = 0;
+      let prevRoutinesDaysTotal = 0
+      let prevGoalsCompleted = 0;
+      let prevGoalsTotal = 0;
+      const numPrevRoutines = previousRoutines.length;
+      for (let i = 0; i < numPrevRoutines; i++) {
+        const prevRoutine = previousRoutines[i];
+        prevRoutinesDaysCompleted += prevRoutine.daysCompleted;
+        prevRoutinesDaysTotal += parseInt(prevRoutine.daysToComplete);
+        prevGoalsCompleted += prevRoutine.completedGoalsCount;
+        prevGoalsTotal += prevRoutine.goals.length;
+      }
+      const prevGoalsFailed = prevGoalsTotal - prevGoalsCompleted;
 
-  const totalGoals = currGoalsTotal + prevGoalsTotal;
-  const totalGoalsCompletedPercentage = totalGoals === 0 ? 0 : Math.round((prevGoalsCompleted / totalGoals) * 100);
+      const totalDaysCompleted = currRoutinesDaysCompleted + prevRoutinesDaysCompleted;
+      const totalDays = currRoutinesDaysTotal + prevRoutinesDaysTotal;
+      const totalDaysCompletedPercentage = totalDays === 0 ? 0 : Math.round((totalDaysCompleted / totalDays) * 100);
 
-  res.render('profile', {
-    navbarTitle: 'Profile',
-    currentRoutines,
-    previousRoutines,
-    numCurrRoutines,
-    numPrevRoutines,
-    totalDaysCompleted,
-    totalDays,
-    totalDaysCompletedPercentage,
-    currGoalsTotal,
-    prevGoalsCompleted,
-    prevGoalsFailed,
-    totalGoals,
-    totalGoalsCompletedPercentage
+      const totalGoals = currGoalsTotal + prevGoalsTotal;
+      const totalGoalsCompletedPercentage = totalGoals === 0 ? 0 : Math.round((prevGoalsCompleted / totalGoals) * 100);
+
+      res.render('profile', {
+        navbarTitle: 'Profile',
+        currentRoutines,
+        previousRoutines,
+        numCurrRoutines,
+        numPrevRoutines,
+        totalDaysCompleted,
+        totalDays,
+        totalDaysCompletedPercentage,
+        currGoalsTotal,
+        prevGoalsCompleted,
+        prevGoalsFailed,
+        totalGoals,
+        totalGoalsCompletedPercentage
+      });
+    })
+
   });
 };
